59f0b1caeaa5ed8dc2b679fda75be292c66b4f68
benni347 <cdrc@skwar.me>
2024-01-16 11:58:05 +0100

    healthcheck

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -2,16 +2,12 @@
 package server
 
 import (
-	"encoding/json"
 	"net/http"
-	"time"
 
 	"github.com/a-h/templ"
-	"github.com/benni347/novum-portfolio/pkg/utils"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/adaptor"
-	"github.com/labstack/echo/v4"
-	log "github.com/sirupsen/logrus"
+	"github.com/gofiber/fiber/v2/middleware/healthcheck"
 )
 
 var serverAddr string
@@ -29,6 +25,8 @@ func NewServer(app *fiber.App) *fiber.App {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return Render(c, IndexComponent("test"))
 	})
+
+	app.Use(healthcheck.New())
 	app.Use(NotFoundMiddleware)
 	return app
 }
@@ -43,49 +41,4 @@ func Render(c *fiber.Ctx, component templ.Component, options ...func(*templ.Comp
 		o(componentHandler)
 	}
 	return adaptor.HTTPHandler(componentHandler)(c)
-}
-
-// WriteJSONError writes error as JSON to ResponseWriter
-func WriteJSONError(w http.ResponseWriter, error string, message string, statusCode int) (int, error) {
-	response := ErrorResponse{Error: error, Message: message}
-	jsonData, err := json.Marshal(response)
-	if err != nil {
-		utils.LogErrorDefaultFormat(appName, "WriteJSONError", err, "Marshalling JSON Data")
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(statusCode)
-	n, err := w.Write(jsonData)
-	if err != nil {
-		utils.LogErrorDefaultFormat(appName, "WriteJSONError", err, "Writing JSON Data")
-	}
-	return n, err
-}
-
-func logRequests(requestersIP string,
-	timeStamp time.Time,
-	method string,
-	httpVersion string,
-	returnValue int,
-	returnSize int,
-	userAgent string,
-) {
-	// Define your time layout
-	layout := "02/Jan/2006:15:04:05 -0700"
-
-	// Use the Format method on timeStamp to get it as a string
-	formattedTime := timeStamp.Format(layout)
-
-	utils.Logger.WithFields(log.Fields{
-		"requestersIp":            requestersIP,
-		"Time":                    formattedTime,
-		"HTTP Method":             method,
-		"HTTP Version":            httpVersion,
-		"HTTP return Status Code": returnValue,
-		"Returned Data Size":      returnSize,
-		"User Agent":              userAgent,
-	}).Info("Log Requests")
-}
-
-func health(c echo.Context) error {
-	return c.String(http.StatusOK, "OK")
 }
